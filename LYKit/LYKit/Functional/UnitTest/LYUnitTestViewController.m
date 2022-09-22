d4f103b40ae48cd3186fba701d812f1ec4744219
zhouluyao <yqzhouluyao@gmail.com>
2022-09-22 20:55:18 +0800

    在控制器中给单元测试进行提示

--- LYKit/LYKit/Functional/UnitTest/LYUnitTestViewController.m
+++ LYKit/LYKit/Functional/UnitTest/LYUnitTestViewController.m
@@ -17,16 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //在LYKitTests.m 文件中对LYCalculatorTool这个类实现的基本运算功能，进行了单元测试
+    //在LYKitTests.m 文件中对LYNetworkRequest这个类实现的网络请求功能，进行了单元测试
+    
+    UIAlertController *alertController1 = [UIAlertController alertControllerWithTitle:@"单元测试" message:@"在LYKitTests.m 文件中对LYCalculatorTool这个类实现的基本运算功能，进行了单元测试" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController1 addAction:([UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self networkRequestUnitTest];
+    }])];
+    [self presentViewController:alertController1 animated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)networkRequestUnitTest {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"单元测试" message:@"在LYKitTests.m 文件中对LYNetworkRequest这个类实现的网络请求功能，进行了单元测试" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:([UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    }])];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
-*/
+
 
 @end
